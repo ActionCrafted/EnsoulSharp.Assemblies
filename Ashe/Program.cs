4f8b3d10471bdef5f032598a986d3a43d8a5d624
ActionCrafted <63268245+ActionCrafted@users.noreply.github.com>
2020-06-03 14:49:16 -0400

    Add files via upload

---
+++ Ashe/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EnsoulSharp.Ashe
+{
+    using System;
+    using EnsoulSharp.SDK;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+
+        private static void OnGameLoad()
+        {
+            if (ObjectManager.Player.CharacterName != "Ashe")
+                return;
+
+            Ashe.OnLoad();
+        }
+    }
+}
